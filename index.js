688ee871edf093fc5e6a42839e90254f07b8c501
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-09-01 11:04:52 +0300

    assignment attempted

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
 // Your code here
+const allWagesFor = 
+function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+      return e.date;
+    });
+  
+    const payable = eligibleDates.reduce(
+      function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d);
+      }.bind(this),
+      0
+    ); 
+  
+    return payable;
+  };
+  
+  let createEmployeeRecord = function (row) {
+    return {
+      firstName: row[0],
+      familyName: row[1],
+      title: row[2],
+      payPerHour: row[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  };
+  
+  let createEmployeeRecords = function (employeeRowData) {
+    return employeeRowData.map(function (row) {
+      return createEmployeeRecord(row);
+    });
+  };
+  
+  let createTimeInEvent = function (dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+  
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return this;
+  };
+  
+  let createTimeOutEvent = function (dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+  
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+    });
+  
+    return this;
+  };
+  
+  let hoursWorkedOnDate = function (soughtDate) {
+    let comingIn = this.timeInEvents.find(function (e) {
+      return e.date === soughtDate;
+    });
+  
+    let goingOut = this.timeOutEvents.find(function (e) {
+      return e.date === soughtDate;
+    });
+  
+    return (goingOut.hour - comingIn.hour) / 100;
+  };
+  
+  let wagesEarnedOnDate = function (dateSought) {
+    let rawWage = hoursWorkedOnDate.call(this, dateSought) * this.payPerHour;
+    return parseFloat(rawWage.toString());
+  };
+  
+  let findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find(function (rec) {
+      return rec.firstName === firstName;
+    });
+  };
+  
+  let calculatePayroll = function (arrayOfEmployeeRecords) {
+    return arrayOfEmployeeRecords.reduce(function (memo, rec) {
+      return memo + allWagesFor.call(rec);
+    }, 0);
+  };
